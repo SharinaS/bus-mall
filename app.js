4fbf9e15e5f7da191f24f9c0f26d8380081e9618
Sharina Stubbs <sharinastubbs@gmail.com>
2019-07-22 19:53:57 -0700

    Completed all portions except for making of list

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var imageOneEl = document.getElementById('item-one');
 var imageTwoEl = document.getElementById('item-two');
 var imageThreeEl = document.getElementById('item-three');
 var ShoppingContainerEl = document.getElementById('shopping-container');
+var listEl = document.getElementById('results');
 
 var allItems = [];
 var itemNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg', 'sweep.png', 'usb.gif'];
 var recentRandomNumbers = [];
+var totalVotes = 0;
 
-// Constructor Function 
+//====== Constructor Function ======
 function Item(name){
   this.name = name.split('.')[0];
   this.filepath = `img/${name}`;
@@ -20,11 +22,24 @@ function Item(name){
   allItems.push(this);
 }
 
-// Instances of Items
+//====== Prototype Function ======
+// Item.prototype.generateString = function(){
+//   // combine string of this.votes + "votes for the " this.name
+//   for(var i = 0; i < itemNames; i++){
+//     this.resultString += this.name[i] + 'votes for the ' + this.name[i];
+//   }
+//   console.log('I am in the generateString protototype function');
+// };
+
+
+
+//===== Instantiations ======
 for(var i = 0; i < itemNames.length; i++){
   new Item(itemNames[i]);
 }
 
+
+// =======Rendering =======
 // make a render function for the randomized images
 function render(){
   // Render Image 1
@@ -52,7 +67,7 @@ function render(){
 // TODO: Write helper function for rendering
 
 function randomNumber(min, max){
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function getUniqueIndex(){
@@ -74,13 +89,32 @@ function getUniqueIndex(){
 function handleClick(){
   // Identify which image was clicked on
   var chosenImg = event.target.title;
+  totalVotes++;
   console.log('my chosen image is ', chosenImg);
 
-  // render all images again to provide new choices
+  for(var i = 0; i < allItems.length; i++){
+    if(allItems[i].name === chosenImg){
+      allItems[i].votes++;
+    }
+  }
+  // turn off event listener after 25 clicks
+  if (totalVotes > 24 ){
+    ShoppingContainerEl.removeEventListener('click', handleClick, true);
+
+    // populate votes using prototype function 
+    // this.generateString();  // <-----------------------------
+    // for(var j = 0; j < allItems.length; j++){
+    //   var liEl = document.createElement('li');
+    //   liEl.textContent = this.resultString[j];
+    //   listEl.appendChild(liEl);
+    // }
+  }
+
+  //render again
   render();
 }
 
 // ===== Event Listener =======
-ShoppingContainerEl.addEventListener('click', handleClick);
+ShoppingContainerEl.addEventListener('click', handleClick, true);
 
 render();
