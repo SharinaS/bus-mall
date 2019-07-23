7184d513f9b868d8d0ddc01220c759a5f86c6345
Sharina Stubbs <sharinastubbs@gmail.com>
2019-07-23 11:27:48 -0700

    figured out, thanks to fellow and supervisors' advice, how to render the list

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ var imageOneEl = document.getElementById('item-one');
 var imageTwoEl = document.getElementById('item-two');
 var imageThreeEl = document.getElementById('item-three');
 var ShoppingContainerEl = document.getElementById('shopping-container');
-// var listEl = document.getElementById('results');
 
 var allItems = [];
 var itemNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg', 'sweep.png', 'usb.gif'];
 var recentRandomNumbers = [];
+
 var totalVotes = 0;
+
+
+var ulEl = document.getElementById('list');
 
 //====== Constructor Function ======
 function Item(name){
@@ -18,20 +21,9 @@ function Item(name){
   this.filepath = `img/${name}`;
   this.votes = 0;
   this.views = 0;
-
+  this.results = '';
   allItems.push(this);
 }
-
-//====== Prototype Function ======
-// Item.prototype.generateString = function(){
-//   // combine string of this.votes + "votes for the " this.name
-//   for(var i = 0; i < itemNames; i++){
-//     this.resultString += this.name[i] + 'votes for the ' + this.name[i];
-//   }
-//   console.log('I am in the generateString protototype function');
-// };
-
-
 
 //===== Instantiations ======
 for(var i = 0; i < itemNames.length; i++){
@@ -40,10 +32,10 @@ for(var i = 0; i < itemNames.length; i++){
 
 
 // =======Rendering =======
-// make a render function for the randomized images
-function render(){
+// make a render function for the randomized images 
+function render(){  // <---------------------------- TODO: DRY
   // Render Image 1
-  var randomIndex = getUniqueIndex();
+  var randomIndex = getUniqueIndex(); 
   allItems[randomIndex].views++;
   imageOneEl.src = allItems[randomIndex].filepath;
   imageOneEl.alt = allItems[randomIndex].name;
@@ -64,26 +56,31 @@ function render(){
 }
 
 // ======= Helper Functions =======
-// TODO: Write helper function for rendering
-
+// create a random number
 function randomNumber(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// get a random and unique index
 function getUniqueIndex(){
-  var randomIndex = randomNumber(0, allItems.length-1);
+  var randomIndex = randomNumber(0, allItems.length-1); // randomIndex gives a random # between 0 and length of items.
 
-  while(recentRandomNumbers.includes(randomIndex)){
-    randomIndex = randomNumber(0, allItems.length-1);
+  while(recentRandomNumbers.includes(randomIndex)){ // includes asks "is this in the array?" returns true/false.
+    randomIndex = randomNumber(0, allItems.length-1); // gets a new random value
   }
-
-  if(recentRandomNumbers.length > 3) {
-    recentRandomNumbers.shift();
+  if(recentRandomNumbers.length > 5) { // we only care about the last three images and the 2 on the page, otherwise we want repeated images. 
+    recentRandomNumbers.shift(); 
   }
-
   recentRandomNumbers.push(randomIndex);
   return randomIndex;
 }
+
+// // render to HTML
+// function addElement(childElType, childContent, parentEl){
+//   var childEl = document.createElement(childElType);
+//   childEl.textContent = childContent;
+//   parentEl.appendChild(childEl);
+// }
 
 // ====== Event handler ======
 function handleClick(){
@@ -97,26 +94,36 @@ function handleClick(){
       allItems[i].votes++;
     }
   }
-  
-  // turn off event listener after 25 clicks
+
   if (totalVotes > 24 ){
-    ShoppingContainerEl.removeEventListener('click', handleClick, true);
-
-  
-    // render vote totals for each instance 
-
-    // this.generateString();  // <-----------------------------
-    // for(var j = 0; j < allItems.length; j++){
-    //   var liEl = document.createElement('li');
-    //   liEl.textContent = this.resultString[j];
-    //   listEl.appendChild(liEl);
-    // }
+    // turn off event listener after 25 clicks
+    ShoppingContainerEl.removeEventListener('click', handleClick, true); // <----- check later if true needed
+    generateList();
   }
-
   render();
 }
+
+//===== Functions =====
+Item.prototype.generateResults = function(){
+  //combine string of this.votes + "votes for the " this.name
+  this.results = `${this.votes} votes for ${this.name}`;
+  var liEl = document.createElement('li');
+  liEl.textContent = this.results;
+  ulEl.appendChild(liEl);
+};
+
+function generateList(){
+  for(var i = 0; i < allItems.length; i++){
+    allItems[i].generateResults();
+  }
+}
+
+
+// each object instance is only responsible for rendering its own <li>. Once the cutoff point of cicks is reached, function generateList is called, which then activates the prototype function. 
 
 // ===== Event Listener =======
 ShoppingContainerEl.addEventListener('click', handleClick, true);
 
+// ===== Render =====
 render();
+
